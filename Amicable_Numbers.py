7127c67ff9fe0344dfc15b80127a79d635fdc49e
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2022-10-31 14:45:32 +0530

    def properfactorsum(n):     s=0     for i in range(1,n):         if n%i==0:             s+=i     return s a=int(input()) b=int(input()) if (properfactorsum(a)==b and properfactorsum(b)==a):     print("Amicable") else:     print("Not Amicable")

--- Amicable_Numbers.py
+++ Amicable_Numbers.py
@@ -1,14 +1,12 @@
-x=int(input())  
-y=int(input())  
-s1=0  
-s2=0  
-for i in range(1,x):  
-    if x%i==0:  
-        s1+=i  
-for j in range(1,y):  
-    if y%j==0:  
-        s2+=j  
-if(s1==y and s2==x):  
-    print("Amicable")  
-else:  
+def properfactorsum(n):
+    s=0
+    for i in range(1,n):
+        if n%i==0:
+            s+=i
+    return s
+a=int(input())
+b=int(input())
+if (properfactorsum(a)==b and properfactorsum(b)==a):
+    print("Amicable")
+else:
     print("Not Amicable")
