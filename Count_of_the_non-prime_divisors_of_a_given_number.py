65e952adc8b9ab117e4212857d158e3ccdf1c341
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-01-01 09:40:17 +0530

    def prime(n):     cnt=0     for j in range(1,n+1):         if n%j==0:             cnt+=1     if cnt==2:         return 0     return 1 a=int(input()) c=0 for i in range(1,a+1):     if a%i==0 and prime(i)!=0:         c+=1 print(c)

--- Count_of_the_non-prime_divisors_of_a_given_number.py
+++ Count_of_the_non-prime_divisors_of_a_given_number.py
@@ -1,14 +1,14 @@
 def prime(n):
-    c=0
-    for i in range(1,n+1):
-        if n%i==0:
-            c+=1
-    if c==2:
+    cnt=0
+    for j in range(1,n+1):
+        if n%j==0:
+            cnt+=1
+    if cnt==2:
         return 0
     return 1
-n=int(input())
+a=int(input())
 c=0
-for i in range(1,n+1):
-    if n%i==0 and prime(i)!=0:
+for i in range(1,a+1):
+    if a%i==0 and prime(i)!=0:
         c+=1
 print(c)
