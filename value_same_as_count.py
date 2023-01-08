739a9ff3130596d4cf83ff19041feffb1249fee8
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-01-08 12:30:17 +0530

    n=int(input()) arr=list(map(int,input().split())) s=[] a=[] c=0 for i in arr:     if i==arr.count(i):         s.append(i) a=set(s) for i in a:     if i%1==0:         c+=1 print(c)

--- value_same_as_count.py
+++ value_same_as_count.py
@@ -3,13 +3,10 @@ arr=list(map(int,input().split()))
 s=[]
 a=[]
 c=0
-d=0
 for i in arr:
     if i==arr.count(i):
         s.append(i)
-for i in s:
-    if i not in a:
-        a.append(i)
+a=set(s)
 for i in a:
     if i%1==0:
         c+=1
