a8e9a6dd64ee8e50572b0f92173989bf7cb41ea1
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-23 13:44:09 +0530

    s = input() a = [] c=0 for i in s:     if i.isalpha():         a.append(i) for i in a:     if a.count(i) == 1:         c+=1 if c==len(a):     print("True") else:     print("False")

--- Isogram_or_not.py
+++ Isogram_or_not.py
@@ -1,6 +1,13 @@
-s=input()
-ss=set(s)
-if(len(s)==len(ss)):
-    print(True)
+s = input()
+a = []
+c=0
+for i in s:
+    if i.isalpha():
+        a.append(i)
+for i in a:
+    if a.count(i) == 1:
+        c+=1
+if c==len(a):
+    print("True")
 else:
-    print(False)
+    print("False")
