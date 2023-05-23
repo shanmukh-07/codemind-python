8252b26a708dd56c6920b836515e97d8daaaa702
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-23 14:32:05 +0530

    r,c = map(int,input().split()) m = [] s=0 for i in range(r):     x = list(map(int,input().split()))     m.append(x)     s+=sum(x) print(s)

--- Sum_of_all_elements_in_a_matrix.py
+++ Sum_of_all_elements_in_a_matrix.py
@@ -1,9 +1,8 @@
 r,c = map(int,input().split())
 m = []
-a=[]
+s=0
 for i in range(r):
     x = list(map(int,input().split()))
     m.append(x)
-for i in m:
-    a.append(sum(i))
-print(sum(a))
+    s+=sum(x)
+print(s)
