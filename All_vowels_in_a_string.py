5ced7b06a1a59d8e3f97f1363a56045138609e07
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-26 12:20:42 +0530

    n = input() l = [] s = 'aeiou' c = 0 for i in n:     if i.isalpha():         l.append(i) b = set(l) for i in s:     if i in b:         c+=1 if c == 5:     print("True") else:     print("False")

--- All_vowels_in_a_string.py
+++ All_vowels_in_a_string.py
@@ -1,19 +1,15 @@
-s=input()
-a,e,i,o,u=0,0,0,0,0
-for v in s:
-    if v=='a':
-        a+=1
-    if v=='e':
-        e+=1
-    if v=='i':
-        i+=1
-    if v=='o':
-        o+=1
-    if v=='u':
-        u+=1
-if a!=0 and e!=0 and i!=0 and o!=0 and u!=0:
-    print(True)
+n = input()
+l = []
+s = 'aeiou'
+c = 0
+for i in n:
+    if i.isalpha():
+        l.append(i)
+b = set(l)
+for i in s:
+    if i in b:
+        c+=1
+if c == 5:
+    print("True")
 else:
-    print(False)
-        
-    
+    print("False")
