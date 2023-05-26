d5936e7632d40664d7bcda82fc64bbd071863c35
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-26 10:56:11 +0530

    n = input() m = n.lower() l = [] d = {} a = [] for i in m:     if i.isalpha():         l.append(i) for i in l:     if i not in d:         d[i]=1     else:         d[i]+=1 for i,j in d.items():     if j == 1:         a.append(i) z = sorted(a) c = ''.join(z) print(c)

--- Distinct_characters.py
+++ Distinct_characters.py
@@ -1,10 +1,19 @@
 n = input()
 m = n.lower()
-l1 = []
+l = []
+d = {}
+a = []
 for i in m:
     if i.isalpha():
-        l1.append(i)
-l2 = set(l1)
-c = sorted(l2)
-d = ''.join(c)
-print(d)
+        l.append(i)
+for i in l:
+    if i not in d:
+        d[i]=1
+    else:
+        d[i]+=1
+for i,j in d.items():
+    if j == 1:
+        a.append(i)
+z = sorted(a)
+c = ''.join(z)
+print(c)
