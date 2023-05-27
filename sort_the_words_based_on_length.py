fc470256b191d6bfd60649ef865cbacec8e0b538
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-27 13:54:17 +0530

    n = input()    # 'tokyo palermo rio nairobi professor denver' m = n.split()  # ['tokyo', 'palermo', 'rio', ' nairobi', 'professor', 'denver'] m.sort()       # ['denver','nairobi', 'palermo', 'professor', 'rio' ,'tokyo'] m.sort(key=len)# ['rio','tokyo','denver','nairobi', 'palermo','professor'] z = ' '.join(m) print(z)

--- sort_the_words_based_on_length.py
+++ sort_the_words_based_on_length.py
@@ -1,6 +1,6 @@
-n = input()
-m = n.split()
-m.sort()
-m.sort(key = len)
-x = ' '.join(m)
-print(x)
+n = input()    # 'tokyo palermo rio nairobi professor denver'
+m = n.split()  # ['tokyo', 'palermo', 'rio', ' nairobi', 'professor', 'denver']
+m.sort()       # ['denver','nairobi', 'palermo', 'professor', 'rio' ,'tokyo']
+m.sort(key=len)# ['rio','tokyo','denver','nairobi', 'palermo','professor']
+z = ' '.join(m)
+print(z)
