45113e0e1e173e8eacf4c221232193798fb7128b
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-30 15:25:03 +0530

    n = input() a = list(n) for i in range(len(a)):     if a[i] == '6':         a[i] ='9'         break x = ''.join(a) print(int(x))

--- Maximum_69_Number.py
+++ Maximum_69_Number.py
@@ -1,15 +1,8 @@
-n=int(input())
-r=0
-while n:
-    r=r*10+n%10
-    n//=10
-m=0
-c=0
-while r:
-    if r%10==6 and c==0:
-        m=m*10+9
-        c=1
-    else:
-        m=m*10+r%10
-    r//=10
-print(m)
+n = input()
+a = list(n)
+for i in range(len(a)):
+    if a[i] == '6':
+        a[i] ='9'
+        break
+x = ''.join(a)
+print(int(x))
