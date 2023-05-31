a27d47d37753eac1e460ccdc1eeef3e617090f8c
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-31 23:57:08 +0530

    def fun(a):     if a<2:         return False     for i in range(2,a):         if a%i == 0:             return False     return True n = int(input()) b = str(n) m = b[::-1] c = int(m) if fun(n) and fun(c):     print("circular prime") if (fun(n) and not fun(c)) or (not fun(n) and fun(c)):     print("prime but not a circular prime") if not fun(n) and not fun(c):     print("not prime")

--- circular_prime.py
+++ circular_prime.py
@@ -1,18 +1,17 @@
-def fun(n):
-    if n<2:
+def fun(a):
+    if a<2:
         return False
-    for i in range(2,n):
-        if n%i == 0:
+    for i in range(2,a):
+        if a%i == 0:
             return False
     return True
-
-n = input()
-m = n[::-1]
-a = int(n)
-b = int(m)
-if fun(a) and fun(b):
+n = int(input())
+b = str(n)
+m = b[::-1]
+c = int(m)
+if fun(n) and fun(c):
     print("circular prime")
-if (fun(a) and not fun(b)) or (not fun(a) and fun(b)):
+if (fun(n) and not fun(c)) or (not fun(n) and fun(c)):
     print("prime but not a circular prime")
-if not fun(a) and not fun(b):
+if not fun(n) and not fun(c):
     print("not prime")
