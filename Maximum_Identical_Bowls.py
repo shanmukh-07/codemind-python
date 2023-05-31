bf72989ba4909209c959a464898eefd73101803f
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-31 22:02:35 +0530

    n = int(input()) arr = list(map(int,input().split())) s = sum(arr) for i in range(n,-1,-1):     if s%i == 0:         print(i)         break

--- Maximum_Identical_Bowls.py
+++ Maximum_Identical_Bowls.py
@@ -1,7 +1,7 @@
-n=int(input())
-arr=list(map(int,input().split()))
-s=sum(arr)
+n = int(input())
+arr = list(map(int,input().split()))
+s = sum(arr)
 for i in range(n,-1,-1):
-    if (s%i)==0:
+    if s%i == 0:
         print(i)
         break
