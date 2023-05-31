f487f4e2d24d5a78b323d66e0248f0831e1f6f1a
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-05-31 23:41:32 +0530

    def fun(n):     if n<2:         return False     for i in range(2,n):         if n%i == 0:             return False     return True
    n = int(input())
    l = []
    l1 = []
    for i in range(n):
        if fun(i):
            l.append(i)
    for i in l:
        if n%i == 0:
            l1.append(i)
    if len(l1) == 0:
        print(-1)
    else:
        print(*l1)

--- Find_two_distinct_prime_numbers_with_given_product.py
+++ Find_two_distinct_prime_numbers_with_given_product.py
@@ -8,14 +8,14 @@ def fun(n):
 
 n = int(input())
 l = []
-for i in range(2,n):
-    for j in range(2,n):
-        if fun(i) and fun(j):
-            if i*j == n:
-                l.append(i)
-                l.append(j)
-m = set(l)
-if len(m)==0:
-    print("-1")
+l1 = []
+for i in range(n):
+    if fun(i):
+        l.append(i)
+for i in l:
+    if n%i == 0:
+        l1.append(i)
+if len(l1) == 0:
+    print(-1)
 else:
-    print(*m)
+    print(*l1)
