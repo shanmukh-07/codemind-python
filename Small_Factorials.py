293d434283b19b13b69abd79cc780edcdc6d3a62
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-06-04 21:06:30 +0530

    for i in range(int(input())):     a=int(input())     p=1     for i in range(1,a+1):         p*=i     print(p)

--- Small_Factorials.py
+++ Small_Factorials.py
@@ -1,6 +1,6 @@
 for i in range(int(input())):
-    x=int(input())
-    f=1
-    for i in range(1,x+1):
-        f*=i
-    print(f)
+    a=int(input())
+    p=1
+    for i in range(1,a+1):
+        p*=i
+    print(p)
