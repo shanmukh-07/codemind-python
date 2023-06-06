a925546c773542f32779977bcc9e350930db56e9
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-06-06 10:39:32 +0530

    n = int(input()) a = 0 b = 1 l = [] l.append(a) l.append(b) while 1:     if len(l) == n:         break     else:         c = a+b         l.append(c)         a = b         b = c l1,l2 = [],[] for i in l:     if i == n:         print(i)         break     elif i<n:         l1.append(i)     elif i>n:         l2.append(i) c = max(l1) d = min(l2) e = abs(c - n) f = abs(d - n) if e>f:     print(d) elif e<f:     print(c) else:     print(c,d)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,13 +1,33 @@
 n = int(input())
 a = 0
 b = 1
-while b<n:
-    c = a+b
-    a = b
-    b = c
-if abs(a-n) < abs(b-n):
-    print(a)
-elif abs(a-n) == abs(b-n):
-    print(a,b)
+l = []
+l.append(a)
+l.append(b)
+while 1:
+    if len(l) == n:
+        break
+    else:
+        c = a+b
+        l.append(c)
+        a = b
+        b = c
+l1,l2 = [],[]
+for i in l:
+    if i == n:
+        print(i)
+        break
+    elif i<n:
+        l1.append(i)
+    elif i>n:
+        l2.append(i)
+c = max(l1)
+d = min(l2)
+e = abs(c - n)
+f = abs(d - n)
+if e>f:
+    print(d)
+elif e<f:
+    print(c)
 else:
-    print(b)
+    print(c,d)
