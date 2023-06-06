bb7c9dc0e488b6819d8d72f8ea141611deddf420
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-06-06 10:28:01 +0530

    def fun(n):     if n<2:         return False     for i in range(2,n):         if n%i == 0:             return False     return True
    for _ in range(int(input())):
        n = int(input())
        a = n+1
        b = n-1
        while 1:
            if fun(a) and fun(b):
                print(min(a,b))
                break
            elif fun(a):
                print(a)
                break
            elif fun(b):
                print(b)
                break
            elif fun(n):
                print(n)
                break
            else:
                a+=1
                b-=1

--- Nearest_Prime.py
+++ Nearest_Prime.py
@@ -1,27 +1,28 @@
 def fun(n):
-    if n < 2:
+    if n<2:
         return False
-    for i in range(2,int(n**0.5)+1):
+    for i in range(2,n):
         if n%i == 0:
             return False
     return True
 
 for _ in range(int(input())):
-    x = int(input())
-    a = x+1
-    b = x-1
+    n = int(input())
+    a = n+1
+    b = n-1
     while 1:
-        if fun(x):
-            print(x)
-            break
         if fun(a) and fun(b):
             print(min(a,b))
             break
-        if fun(a):
+        elif fun(a):
             print(a)
             break
-        if fun(b):
+        elif fun(b):
             print(b)
             break
-        a+=1
-        b-=1
+        elif fun(n):
+            print(n)
+            break
+        else:
+            a+=1
+            b-=1
