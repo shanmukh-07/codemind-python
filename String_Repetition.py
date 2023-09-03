9607d14660163af7cb7c35c6bede14324fa51aa8
shanmukh-07 <97872565+shanmukh-07@users.noreply.github.com>
2023-09-03 15:32:05 +0530

    s = input() n = int(input()) a1 = n//len(s) a2 = n%len(s) c1 = s.count('a') * a1 z = s[:a2] c2 = z.count('a') print(c1+c2)

--- String_Repetition.py
+++ String_Repetition.py
@@ -1,9 +1,8 @@
-n = input()
-n = n.replace(' ','')
-k = int(input())
-a1 = k//len(n) 
-a2 = k%len(n) 
-c1 = (n.count('a'))*a1 
-z = n[:a2] 
-c2 = z.count('a') 
+s = input()
+n = int(input())
+a1 = n//len(s)
+a2 = n%len(s)
+c1 = s.count('a') * a1
+z = s[:a2]
+c2 = z.count('a')
 print(c1+c2)
